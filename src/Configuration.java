2b99347f67f4ee7ee2de9d82f946f87799c9fcd9
Pahan Supurna <pahansupurna2006@gmail.com>
2024-11-26 23:46:28 +0530

    Updated the Main and Configuration classes

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,22 +1,25 @@
 public class Configuration {
+    //Initializing the attributes
     private int totalTickets;
-    private int ticketReleaseRate;
+    private int releaseRate;
     private int retrievalRate;
     private int ticketCapacity;
 
-    public Configuration(){
-        totalTickets = 120;
-        ticketReleaseRate = 5;
-        retrievalRate = 2;
-        ticketCapacity = 100;
+    //Constructor
+    public Configuration(int totalTickets, int retrievalRate, int releaseRate, int ticketCapacity){
+        this.totalTickets = totalTickets;
+        this.releaseRate = releaseRate;
+        this.retrievalRate = retrievalRate;
+        this.ticketCapacity = ticketCapacity;
     }
 
+    //Getters of the attributes
     public int getTotalTickets(){
         return totalTickets;
     }
 
-    public int getTicketReleaseRate(){
-        return ticketReleaseRate;
+    public int getReleaseRate(){
+        return releaseRate;
     }
 
     public int getRetrievalRate(){
@@ -25,5 +28,40 @@ public class Configuration {
 
     public int getTicketCapacity(){
         return ticketCapacity;
+    }
+
+    //Setters of the attributes
+    public void setTotalTickets(int totalTickets){
+        this.totalTickets = totalTickets;
+    }
+
+    public void setReleaseRate(int releaseRate){
+        this.releaseRate = releaseRate;
+    }
+
+    public void setRetrievalRate(int retrievalRate){
+        this.retrievalRate = retrievalRate;
+    }
+
+    public void setTicketCapacity(int ticketCapacity){
+        this.ticketCapacity = ticketCapacity;
+    }
+
+    //Method for validate the user inputs
+    public boolean validate(){
+        if (totalTickets<0 || ticketCapacity<0 || retrievalRate<0 || releaseRate<0){
+            System.out.println("Please enter a positive number.");
+            return false;
+        }
+        if (totalTickets>ticketCapacity){
+            System.out.println("Please enter a number smaller than the total ticket count.");
+            return false;
+        }
+        if (retrievalRate<releaseRate){
+            System.out.println("Ticket release rate should be higher than ticket retrieval rate.");
+            return false;
+        }
+
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public void main{
-    public static void main(String [] args){
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the total number of tickets? ");
+        int totalTickets = scanner.nextInt();
+
+        System.out.println("Enter the Maximum number of tickets that can be stored? ");
+        int ticketCapacity = scanner.nextInt();
+
+        System.out.println("Enter the ticket retrieval rate? ");
+        int retrievalRate = scanner.nextInt();
+
+        System.out.println("Enter the ticket release rate? ");
+        int releaseRate = scanner.nextInt();
+
+        Configuration configuration = new Configuration(totalTickets, ticketCapacity, retrievalRate, releaseRate);
+
+        if (configuration.validate()) {
+            System.out.println("Inputs are valid.");
+        } else {
+            System.out.println("Inputs are invalid.");
+        }
+        
+        scanner.close();
     }
 }
