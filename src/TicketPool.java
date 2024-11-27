45401f38101d17f255821218c9faef119fd692e0
Pahan Supurna <pahansupurna2006@gmail.com>
2024-11-27 23:51:20 +0530

    Updated the TicketPool class

--- src/TicketPool.java
+++ src/TicketPool.java
@@ -1,3 +1,13 @@
+import java.util.LinkedList;
+
 public class TicketPool {
+    private final LinkedList<Integer> tickets;
+    private final int ticketCapacity;
+
+    public TicketPool(Configuration configuration) {
+        this.tickets = new LinkedList<>();
+        this.ticketCapacity = configuration.getTicketCapacity();
+    }
+
 
 }
