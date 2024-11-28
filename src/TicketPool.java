ee236013844ef96bde13016f9b65a5ac2ad7123f
Pahan Supurna <pahansupurna2006@gmail.com>
2024-11-28 23:56:12 +0530

    Updated the structure of TicketPool class

--- src/TicketPool.java
+++ src/TicketPool.java
@@ -1,13 +1,16 @@
-import java.util.LinkedList;
-
 public class TicketPool {
-    private final LinkedList<Integer> tickets;
-    private final int ticketCapacity;
+    private int ticketCapacity;
+    private int totalTickets;
+    private int[] ticketStoreArray;
 
-    public TicketPool(Configuration configuration) {
-        this.tickets = new LinkedList<>();
+    public TicketPool(Configuration configuration){
         this.ticketCapacity = configuration.getTicketCapacity();
+        this.ticketStoreArray = new int[configuration.getTotalTickets()];
     }
 
+    public synchronized void addTicket(int ticket){
+        if(ticketStoreArray.length < ticketCapacity){
 
+        }
+    }
 }
