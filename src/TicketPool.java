4bd63b2290fc6f21496f612907b89dcc7658946c
Pahan Supurna <pahansupurna2006@gmail.com>
2024-11-29 23:39:00 +0530

    Completed the basic structure of TicketPool class with comments

--- src/TicketPool.java
+++ src/TicketPool.java
@@ -1,16 +1,45 @@
-public class TicketPool {
-    private int ticketCapacity;
-    private int totalTickets;
-    private int[] ticketStoreArray;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public TicketPool(Configuration configuration){
-        this.ticketCapacity = configuration.getTicketCapacity();
-        this.ticketStoreArray = new int[configuration.getTotalTickets()];
+//Initialized the class
+public class TicketPool {
+    private final Queue<String> ticketQueue; //Queue to store tickets in the system
+    private final Configuration configuration; // Object of Configuration class
+    private int ticketNumber; //To get the ticket number for displaying.
+
+    // Constructor
+    public TicketPool (Configuration configuration){
+        this.ticketQueue = new LinkedList<>();
+        this.configuration = configuration;
+        this.ticketNumber = 0;
     }
 
-    public synchronized void addTicket(int ticket){
-        if(ticketStoreArray.length < ticketCapacity){
-
+    // Method for adding the tickets to the system by Vendors.
+    public synchronized void addTicket() throws InterruptedException{
+        //checks if the number of tickets in the system is higher than the maximum capacity.
+        while (ticketQueue.size() >= configuration.getTicketCapacity()){
+            System.out.println("Ticket system is full, please wait till a customer buys a ticket.");
+            wait(); //waits till a customer buys a ticket
         }
+
+        ticketNumber ++;
+        String ticket = "Ticket Number " + ticketNumber; //creates the ticket name to display
+        ticketQueue.offer(ticket); //Adds the ticket to the queue
+        System.out.println(ticket + "Successfully added to the system. Number of tickets in the system = "+ ticketQueue.size());
+        notifyAll(); //Notifies the customers about the added tickets.
+    }
+
+    // Method for buying tickets in the system by customers.
+    public synchronized String buyTicket() throws InterruptedException{
+        // Checks if there is no available tickets in the system.
+        while (ticketQueue.isEmpty()){
+            System.out.println("There are no available tickets in the system. Please wait!");
+            wait(); //waits till a vendor adds tickets to the system
+        }
+
+        String ticket = ticketQueue.poll(); //Removes the ticket from the system
+        System.out.println(ticket + "is successfully purchased. Number of tickets remaining in the system = "+ ticketQueue.size());
+        notifyAll(); //Notifies the vendors about the available ticket slot
+        return ticket;
     }
 }
