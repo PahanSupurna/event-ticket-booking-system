e668fe0be38a014121b17a6397b2e4daf0768d34
Pahan Supurna <pahansupurna2006@gmail.com>
2024-12-02 00:48:27 +0530

    Updated the Vendor class to release tickets with a delay

--- src/main/java/com/example/event_ticket_booking_system/Vendor.java
+++ src/main/java/com/example/event_ticket_booking_system/Vendor.java
@@ -2,13 +2,27 @@ package com.example.event_ticket_booking_system;
 
 public class Vendor implements Runnable {
     private final TicketPool ticketPool;
-    private final Configuration configuration;
-    private final int vendorId;
+    private final int totalTickets;
+    private final int releaseRate;
 
     // Constructor
-    public Vendor(TicketPool ticketPool, Configuration configuration, int vendorId) {
+    public Vendor(TicketPool ticketPool, Configuration configuration) {
         this.ticketPool = ticketPool;
-        this.configuration = configuration;
-        this.vendorId = vendorId;
+        this.releaseRate = configuration.getReleaseRate();
+        this.totalTickets = configuration.getTotalTickets();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < totalTickets; i++) {
+            try {
+                //Adds the ticket to the pool
+                ticketPool.addTicket();
+                //Waiting time before releasing the next ticket
+                Thread.sleep(releaseRate * 1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
